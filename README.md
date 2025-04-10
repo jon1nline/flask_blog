11fe613974832cf89c9a84c623c1721606eef048
jon1nline <www.jhonattan@hotmail.com>
2025-04-09 21:14:58 -0300

    database and tables

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# flask_blog
+## Projeto Python + flask
+
+
+
+
+
+## Documentação
+
+1st commit - files on github
+
+2nd commit - create the database and tables
+
+
+## Autores
+
+- [@Jon](https://www.github.com/jon1nline)
--- board/__init__.py
+++ board/__init__.py
@@ -1,6 +1,22 @@
-from flask import Flask 
+import os
+import click
+from flask import Flask , current_app
 from board import pages
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy import Integer, String, DateTime, func, ForeignKey
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str]
 
 def create_app():
     app = Flask(__name__)
@@ -8,3 +24,32 @@ def create_app():
 
     return app
 
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    
+    click.echo('Initialized the database.')
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI='sqlite:///cadastro.db',
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    
+    #gegistra o comando cli
+    app.cli.add_command(init_db_command)
+    #inicia a extensão
+    db.init_app(app)
+    return app
